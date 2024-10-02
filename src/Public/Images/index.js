0340d17249ce5313be9bf2577ba9eb4cf42efb9c
codeforme <deveshtiwari2511@gmail.com>
2024-10-03 00:41:41 +0530

    Home page done

---
+++ src/Public/Images/index.js
@@ -0,0 +1,4 @@
+import logo from "./Group.svg";
+import upperDesignItems from "./彩色@2x.svg";
+import rightArrow from "./arrow-sm-right-svgrepo-com.svg";
+export { logo, upperDesignItems, rightArrow };
---
+++ src/app/Components/UI/Btn.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import rightArrow from "../../../Public/Images/arrow-sm-right-svgrepo-com.svg";
+import Image from "next/image";
+
+interface ButtonProps {
+  text: string;
+  className?: string;
+}
+
+const Btn = ({ text, className }: ButtonProps) => {
+  return (
+    <div
+      className={`bg-[#FF3B3F] relative flex rounded-[50px] font-black text-2xl shadow-md items-center justify-center cursor-pointer transition-transform duration-200 ease-in-out  hover:bg-[#f71216] hover:scale-105 ${className}`}
+    >
+      <div>{text}</div>
+      {text === "Next" && (
+        <Image
+          className=" absolute right-5"
+          src={rightArrow}
+          alt="right-arrow"
+          width={30}
+          height={30}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Btn;
