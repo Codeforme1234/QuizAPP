09a8bf64ef190143e9dc4c87f8e7d90cf56bd4fd
codeforme <deveshtiwari2511@gmail.com>
2024-10-05 19:52:07 +0530

    fixes - nextjs.config.js added images urls

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   output: "standalone",
+  images: {
+    domains: [
+      "media.licdn.com",
+      "i.gifer.com",
+      "media1.tenor.com",
+      "static.wikia.nocookie.net",
+    ],
+  },
   env: {
     HOSTNAME: "0.0.0.0",
   },
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.licdn.com","i.gifer.com","media1.tenor.com","static.wikia.nocookie.net"],
+    domains: [
+      "media.licdn.com",
+      "i.gifer.com",
+      "media1.tenor.com",
+      "static.wikia.nocookie.net",
+    ],
   },
 };
 
